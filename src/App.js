ef55bf41277df6358889947746bdd0d2485ccfb1
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2021-12-27 16:53:58 -0500

    Index js test completed

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Footer from "./components/common/Footer";
 import Home from "./components/home/Home";
 import StaffList from "./components/staff/StaffList";
 import PetsList from "./components/pets/PetsList";
+import { Link, Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 
 /*
   Data
@@ -31,13 +32,26 @@ class App extends React.Component {
   render() {
     const { employees, owners, pets } = this.state;
     return (
+      <Router>
       <div className="wrapper">
         <Nav />
+        <Link to="/"></Link>
+        <Link to="/staff"></Link>
+        <Link to="/pets"></Link>
+        <Switch>
+        <Route exact path="/">
         <Home employees={employees} owners={owners} pets={pets} />
+        </Route>
+        <Route path="/staff">
         <StaffList employees={employees} />
+        </Route>
+        <Route path="/pets">
         <PetsList pets={pets} />
+        </Route>
+        </Switch>
         <Footer />
       </div>
+      </Router>
     );
   }
 }
